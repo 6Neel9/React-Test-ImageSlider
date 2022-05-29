9d5f4ab92754a67e4f76be41d032382e7c51bae1
6Neel9 <nilmandal300@gmail.com>
2022-05-29 19:44:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Slider from './Components/Slider/Slider'
+
+
+function App() {
+  return (
+    <>
+    <u><h2>Carousel Image Application</h2></u>
+    
+      <Slider />
+    </>
+  );
+}
+
+export default App;
